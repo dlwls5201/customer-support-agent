28681f9b3432fd892e48678c6243dbf2019d3f1e
dlwls5201 <dlwls5201@naver.com>
2025-11-10 17:48:24 +0900

    handoffs

--- my_agents/triage_agent.py
+++ my_agents/triage_agent.py
@@ -1,11 +1,19 @@
+import streamlit as st
 from agents import (
     Agent,
     RunContextWrapper,
     input_guardrail,
     Runner,
     GuardrailFunctionOutput,
+    handoff,
 )
-from models import UserAccountContext, InputGuardRailOutput
+from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
+from models import UserAccountContext, InputGuardRailOutput, HandoffData
+from my_agents.account_agent import account_agent
+from my_agents.billing_agent import billing_agent
+from my_agents.order_agent import order_agent
+from my_agents.technical_agent import technical_agent
+
 
 input_guardrail_agent = Agent(
     name="Input Guardrail Agent",
@@ -41,6 +49,8 @@ def dynamic_triage_agent_instructions(
     agent: Agent[UserAccountContext],
 ):
     return f"""
+     {RECOMMENDED_PROMPT_PREFIX}
+
     You are a customer support agent. You ONLY help customers with their questions about their User Account, Billing, Orders, or Technical Support.
     You call customers by their name.
     
@@ -94,10 +104,39 @@ def dynamic_triage_agent_instructions(
     """
 
 
+def handle_handoff(
+    wrapper: RunContextWrapper[UserAccountContext],
+    input_data: HandoffData,
+):
+    with st.sidebar:
+        st.write(
+            f"""
+            Handing off to {input_data.to_agent_name}
+            Reason: {input_data.reason}
+            Issue Type: {input_data.issue_type}
+            Description: {input_data.issue_description}
+        """
+        )
+
+
+def make_handoff(agent):
+    return handoff(
+        agent=agent,
+        on_handoff=handle_handoff,
+        input_type=HandoffData,
+    )
+
+
 triage_agent = Agent[UserAccountContext](
     name="Triage Agent",
     instructions=dynamic_triage_agent_instructions,
     input_guardrails=[
         off_topic_guardrail,
+    ],
+    handoffs=[
+        make_handoff(account_agent),
+        make_handoff(billing_agent),
+        make_handoff(order_agent),
+        make_handoff(technical_agent),
     ],
 )
